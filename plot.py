ed997985ce530aaec6f988555e181ffbcab3a35d
Stefan PC <stefan_held@hotmail.de>
2023-06-03 20:05:49 +0200

    good model - trained from 90- 95% vs random in selfplay

--- plot.py
+++ plot.py
@@ -9,8 +9,8 @@ arrayOfAverages = []
 with open("models/averages.txt", "r") as f:
     arr = [eval(line) for line in f.readlines()]
 arrayOfAverages = arr
-print(arr)
-
+#print(arr)
+#print("Arr Print End;")
 averages = [sum(array) / len(array) if len(array) > 0 else 0 for array in arrayOfAverages]
 
 ##plot averages as a line graph
@@ -48,6 +48,12 @@ def plotInteractive(data):
 
 
     leg = plt.legend()
+    # Make lines invisible by default
+    for line in lines:
+        line.set_visible(False)
+
+    # Make the first line visible, as it was the first to be trained
+    lines[0].set_visible(True)
 
     lined = {}  # Will map legend lines to original lines
     for legline, origline in zip(leg.get_lines(), lines):
@@ -74,7 +80,6 @@ def plotInteractive(data):
 
     plt.grid()
     plt.show()
-plotInteractive(data)
 
 
 
@@ -125,6 +130,51 @@ def plotAverageWithBigDots(data):
     plt.grid()
     plt.show()
 
-plotBaseAv(avg_rewards)
+
+
+def plotAvav(averages):
+    # create an array of the same length for the x values (time)
+    time = np.arange(len(averages))
+
+    # calculate the line of best fit
+    coefficients = np.polyfit(time, averages, 1)
+    polynomial = np.poly1d(coefficients)
+
+    # create the line of best fit
+    line_of_best_fit = polynomial(time)
+
+    # plot your data
+    plt.plot(time, averages, 'o')
+    plt.plot(time, line_of_best_fit)
+    plt.show()
+
+
+length = 0
+prevlength = 0
+line = 0
+for array in arr:
+    line+=1
+    length = len(array) - prevlength
+    if(length != 1):
+        print("Error in Averages at line: ", line)
+    prevlength = len(array)
+
+
+
 #plotavSmallDots(data)
 #plotAverageWithBigDots(data)
+
+#plotInteractive(data)
+plotBaseAv(avg_rewards)
+plotAvav(avg_rewards[0:])
+
+rewards = avg_rewards
+# Compute the cumulative sum and divide by the number of elements so far at each point
+avg_rewards = np.cumsum(rewards) / np.arange(1, len(rewards) + 1)
+
+# Plotting
+plt.plot(avg_rewards)
+plt.title('Average Reward Growth Over Time')
+plt.xlabel('Time')
+plt.ylabel('Average Reward')
+plt.show()
